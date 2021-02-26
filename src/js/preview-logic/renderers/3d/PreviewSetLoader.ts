31d8e54f2d27087b26f3ed333fdb368a38906cc6
Bas Horsting <bas@greetz.com>
2021-02-26 11:37:02 +0100

    Add more examples

--- src/js/preview-logic/renderers/3d/PreviewSetLoader.ts
+++ src/js/preview-logic/renderers/3d/PreviewSetLoader.ts
@@ -3,9 +3,9 @@ import { PositionMapperWebGl } from './PositionMapperWebGl';
 export class PreviewSetLoader {
   constructor(private readonly baseUrl: string) {}
   async loadSet(id: number, textureUrl, positionMapper: PositionMapperWebGl) {
-    const baseImageUrl = `${this.baseUrl}/${id}/images/scene`;
-    let positionUrl = `${this.baseUrl}/${id}/images/uv`;
-    let reflectionMapUrl = `${this.baseUrl}/${id}/images/meta`;
+    const baseImageUrl = `${this.baseUrl}/${id}/scene`;
+    let positionUrl = `${this.baseUrl}/${id}/uv`;
+    let reflectionMapUrl = `${this.baseUrl}/${id}/meta`;
     await positionMapper.load(
       baseImageUrl,
       textureUrl,
